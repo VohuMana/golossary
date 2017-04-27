10f4652b057b4d5b062cfb7f549abe534b95daf4
Vohumana <david.linden.james@gmail.com>
2017-04-26 21:57:10 -0700

    Adding some styling, starting with the bootstrap basic template

---
+++ static/js/index.js
@@ -0,0 +1,26 @@
+function onSuccess(data, textStatus, jqXHR ) {
+    for (var word in data) {
+        if (data[word] !== null) {
+            var newDiv = document.createElement('div');
+            var header = document.createElement('h3');
+            var definition = document.createElement('p');
+            header.innerHTML = word;
+            definition.innerHTML = data[word];
+            newDiv.appendChild(header);
+            newDiv.appendChild(definition);
+            document.body.appendChild(newDiv);
+        }
+    }
+}
+
+function onSubmit() {
+    var inputTextArea = document.getElementById("inputTextArea");
+
+    $.ajax({
+        type: "POST",
+        url: "api/define",
+        data: inputTextArea.value,
+        success: onSuccess,
+        dataType: "json"
+    });
+}
