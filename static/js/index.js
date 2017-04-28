581fb94ae0e3b3361c8e46ccaa2c323e21eff92a
Vohumana <david.linden.james@gmail.com>
2017-04-27 21:05:35 -0700

    Lot's of style improvements

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,8 @@
 function onSuccess(data, textStatus, jqXHR ) {
+    removeAllChildrenFromNode(document.getElementById("definitionContainer"));
+    removeAllChildrenFromNode(document.getElementById("missingDefinitionsContainer"));
+    
+    var wordsWithNoDefinition = [];
     for (var word in data) {
         if (data[word] !== null) {
             var newDiv = document.createElement('div');
@@ -8,8 +12,30 @@ function onSuccess(data, textStatus, jqXHR ) {
             definition.innerHTML = data[word];
             newDiv.appendChild(header);
             newDiv.appendChild(definition);
-            document.getElementById("mainContainer").appendChild(newDiv);
+            document.getElementById("definitionContainer").appendChild(newDiv);
+        } else {
+            wordsWithNoDefinition.push(word);
         }
+    }
+    
+    var noDefinitionContainer = document.getElementById("missingDefinitionsContainer");
+    var newHeader = document.createElement('h2');
+    newHeader.innerHTML = "Could not find definitions for these words:";
+    noDefinitionContainer.appendChild(newHeader);
+
+    var newList = document.createElement('ul');
+    for (var index in wordsWithNoDefinition) {
+        var listItem = document.createElement('li');
+        listItem.innerHTML = wordsWithNoDefinition[index];
+        newList.appendChild(listItem);
+    }
+
+    noDefinitionContainer.appendChild(newList);
+}
+
+function removeAllChildrenFromNode(node) {
+    while (node.firstChild) {
+        node.removeChild(node.firstChild);
     }
 }
 
